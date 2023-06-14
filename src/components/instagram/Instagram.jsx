4d41985c48f0b7d892bd1106f28dcc5de023b26e
Eismer Lobaina <eismerlobaina@gmail.com>
2023-06-13 21:36:28 -0400

    Desplegando una nueva version

--- src/components/instagram/Instagram.jsx
+++ src/components/instagram/Instagram.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Instagram = () => {
     const path = "/assets/main/home-8.png";
-  const imagePath = `${process.env.PUBLIC_URL}.${path}`;
+  const imagePath = `${window.location.origin}.${path}`;
 
   return (
     <div className="mb-5">
@@ -11,11 +11,11 @@ export const Instagram = () => {
         <img src={imagePath} className="img" alt="" />
       </div>
       <div className="row justify-content-center">
-        <img src={`${process.env.PUBLIC_URL}/assets/main/instagram/84981049_620107085435507_4260875787090681190_nlow.jpg`} className="img col-2" alt="instagram" />
-        <img src={`${process.env.PUBLIC_URL}/assets/main/instagram/85051426_2060664737412512_8458893884651247910_nlow.jpg`} className="img col-2" alt="instagram" />
-        <img src={`${process.env.PUBLIC_URL}/assets/main/instagram/87339849_530805007551424_292323017375800029_nlow.jpg`} className="img col-2" alt="instagram" />
-        <img src={`${process.env.PUBLIC_URL}/assets/main/instagram/85069033_185901059177965_6767010623440980864_nlow.jpg`} className="img col-2" alt="instagram" />
-        <img src={`${process.env.PUBLIC_URL}/assets/main/instagram/87413583_2660130777540405_5722961474466513534_nlow.jpg`} className="img col-2" alt="instagram" />
+        <img src={`${window.location.origin}/assets/main/instagram/84981049_620107085435507_4260875787090681190_nlow.jpg`} className="img col-2" alt="instagram" />
+        <img src={`${window.location.origin}/assets/main/instagram/85051426_2060664737412512_8458893884651247910_nlow.jpg`} className="img col-2" alt="instagram" />
+        <img src={`${window.location.origin}/assets/main/instagram/87339849_530805007551424_292323017375800029_nlow.jpg`} className="img col-2" alt="instagram" />
+        <img src={`${window.location.origin}/assets/main/instagram/85069033_185901059177965_6767010623440980864_nlow.jpg`} className="img col-2" alt="instagram" />
+        <img src={`${window.location.origin}/assets/main/instagram/87413583_2660130777540405_5722961474466513534_nlow.jpg`} className="img col-2" alt="instagram" />
 
       </div>
     </div>
