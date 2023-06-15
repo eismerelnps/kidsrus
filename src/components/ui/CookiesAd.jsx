02f42e06670212d388da0f5707d4844f11bb8050
Eismer Lobaina <eismerlobaina@gmail.com>
2023-06-14 22:51:17 -0400

    desplegando version 1.0

--- src/components/ui/CookiesAd.jsx
+++ src/components/ui/CookiesAd.jsx
@@ -3,11 +3,15 @@ import React from "react";
 export const CookiesAd = () => {
   return (
     <div className="d-flex align-items-center justify-content-center p-1">
+      <div className="d-inline ">
       <p className="font_gilda_display text-dark">
         This website uses cookies to improve your experience. We'll assume
         you're ok this, but you can opt-out if you wish.
       </p>
+      
+      </div>
       <button className="btn btn-dark text-light rounded-0">Accept</button>
+
     </div>
   );
 };
