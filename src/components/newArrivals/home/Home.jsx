99cf81287afd05edd3b88ba691e2f0f31f2bfa6b
Eismer Lobaina <eismerlobaina@gmail.com>
2023-06-29 17:32:05 -0400

    deploying a new version

--- src/components/newArrivals/home/Home.jsx
+++ src/components/newArrivals/home/Home.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import './home.css'
+import "./home.css";
 
 export const Home = () => {
   return (
     <div className="row mb-5">
-    <div className="col-8 p-1">
-      <div className="position-relative">
-        <img
-          src={`../../../assets/main/slider-1.jpg`}
-          className="img-fluid"
-          alt="page_logo"
-        />
-        <div className="position-absolute bottom-0 start-0 p-3">
-          <h1 className="text-white">Jumpsuits</h1>
-          <p className="text-white mb-3">Confortable clothes for you little babies</p>
-          <div className="bg-light p-1 d-inline-block">
-          <button className="discover-btn btn color_mate_blue font_francois_one">DISCOVER</button>
+      <div className="col-12 col-xl-8 p-1">
+        <div className="position-relative">
+          <img
+            src={`../../../assets/main/slider-1.jpg`}
+            className="img-fluid"
+            alt="page_logo"
+          />
+          <div className="position-absolute bottom-0 start-0 p-3">
+            <h1 className="text-white">Jumpsuits</h1>
+            <p className="text-white mb-3">
+              Confortable clothes for you little babies
+            </p>
+            <div className="bg-light p-1 d-inline-block">
+              <button className="discover-btn btn color_mate_blue font_francois_one">
+                DISCOVER
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="col-4 p-1">
-      <img
-        src={`../../../assets/main/girls-power.jpg`}
-        className="img-fluid"
-        alt="page_logo"
-      />
-      <div className="text-center p-0 m-0">
-        <p className="font_francois_one color_mate_blue m-0"> GIRL POWER</p>
-        <p className="font_gilda_display"> For a colorful summer </p>
+      <div className="col-12 col-xl-4 p-1">
+        <img
+          src={`../../../assets/main/girls-power.jpg`}
+          className="img-fluid"
+          alt="page_logo"
+        />
+        <div className="text-center p-0 m-0">
+          <p className="font_francois_one color_mate_blue m-0"> GIRL POWER</p>
+          <p className="font_gilda_display"> For a colorful summer </p>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
