ae7f3398e422fbcdcbcb51130723a307936b01d0
Eismer Lobaina <eismerlobaina@gmail.com>
2023-06-30 21:40:38 -0400

    fix: -fixing intagram component img path importing with require.context

--- src/components/newArrivals/instagram/Instagram.jsx
+++ src/components/newArrivals/instagram/Instagram.jsx
@@ -1,31 +1,19 @@
 import React from "react";
 
-import './instagram.css'
+import "./instagram.css";
 
-import submark from "../../../assets/main/home-8.png"
-
-import img1 from "../../../assets/main/instagram/84981049_620107085435507_4260875787090681190_nlow.jpg";
-import img2 from     "../../../assets/main/instagram/85051426_2060664737412512_8458893884651247910_nlow.jpg";
-
-import img3 from  "../../../assets/main/instagram/87339849_530805007551424_292323017375800029_nlow.jpg"
-import img4 from  "../../../assets/main/instagram/85069033_185901059177965_6767010623440980864_nlow.jpg"
-import img5 from  "../../../assets/main/instagram/87413583_2660130777540405_5722961474466513534_nlow.jpg"
-
+import submark from "../../../assets/main/home-8.png";
+const imagesSRC = require.context("../../../assets/main/instagram", true);
 
 export const Instagram = () => {
- 
 
   const images = [
-
-    img1, img2, img3, img4, img5
-    // "../../../assets/main/instagram/84981049_620107085435507_4260875787090681190_nlow.jpg",
-    // "../../../assets/main/instagram/85051426_2060664737412512_8458893884651247910_nlow.jpg",
-    // "../../../assets/main/instagram/87339849_530805007551424_292323017375800029_nlow.jpg",
-    // "../../../assets/main/instagram/85069033_185901059177965_6767010623440980864_nlow.jpg",
-    // "../../../assets/main/instagram/87413583_2660130777540405_5722961474466513534_nlow.jpg",
+    "84981049_620107085435507_4260875787090681190_nlow.jpg",
+    "85051426_2060664737412512_8458893884651247910_nlow.jpg",
+    "87339849_530805007551424_292323017375800029_nlow.jpg",
+    "85069033_185901059177965_6767010623440980864_nlow.jpg",
+    "87413583_2660130777540405_5722961474466513534_nlow.jpg",
   ];
-
-  
 
   return (
     <div className="mb-5">
@@ -37,13 +25,14 @@ export const Instagram = () => {
       </div>
       <div className="row justify-content-center">
         {images.map((img, i) => (
-          <img
-          style={{cursor: "pointer"}}
-          key={i}
-            src={img}
-            className="instagram_images img col-2"
-            alt="instagram"
-          />
+          <div key={i} className="col-6 col-xl-2 mb-3">
+            <img
+              style={{ cursor: "pointer" }}
+              src={imagesSRC(`./${img}`)}
+              className="instagram_images img-fluid "
+              alt="instagram"
+            />
+          </div>
         ))}
       </div>
     </div>
