55b730736df76ad38acd3004adab91a7300cbf4c
Eismer Lobaina <eismerlobaina@gmail.com>
2023-07-07 15:51:59 -0400

    feat: -the header component WishList and Cart icons, comsumes its amount directly from user context

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -5,38 +5,38 @@ import logo from "../../assets/main/logo.png";
 
 import "./ui.css";
 import { DrawerAppBar } from "../newArrivals/navbar/DrawerAppBar";
+import { useContext } from "react";
+import { AuthContext } from "../../auth/authContext";
 
 export const Header = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="mb-5">
       <div className="text-center">
         <img src={logo} className="img mb-5" alt="page_logo" />
       </div>
-      {/* <div className="text-end">
-        <i className="fa-regular fa-heart p-2 color_mate_blue"></i>
 
-        <i className="fa-solid fa-cart-shopping p-2 color_mate_blue position-relative">
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            2<span className="visually-hidden">Products in cart</span>
-          </span>
-        </i>
-
-        <i className="fa-solid fa-magnifying-glass p-2 color_mate_blue"></i>
-      </div> */}
-      {/* <DrawerAppBar /> */}
       <ul className="nav justify-content-center mb-5 ">
         {nav.map(({ title, navTo }, index) => (
-          <NavItem key={title} title={title} navTo={navTo} index={index}/>
+          <NavItem key={title} title={title} navTo={navTo} index={index} />
         ))}
-        <i className="fa-regular fa-heart p-2 color_mate_blue"></i>
 
-        <i className="fa-solid fa-cart-shopping p-2 color_mate_blue position-relative">
+        <i className="fa-regular fa-heart p-2 color_mate_blue position-relative ">
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            2<span className="visually-hidden">Products in cart</span>
+            {user.wishList.count}
+            <span className="visually-hidden">Products in wish-list</span>
           </span>
         </i>
 
-        <i className="fa-solid fa-magnifying-glass p-2 color_mate_blue"></i>
+        <i className="fa-solid fa-cart-shopping p-2 color_mate_blue position-relative">
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {user.cart.count}
+            <span className="visually-hidden">Products in cart</span>
+          </span>
+        </i>
+
+        <i className="fa-solid fa-magnifying-glass p-2 color_mate_blue" ></i>
       </ul>
     </div>
   );
