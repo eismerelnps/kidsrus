5b7cc16dcd20ca347126cbbd2dbf6313255037c0
Eismer Lobaina <eismerlobaina@gmail.com>
2023-07-09 01:32:56 -0400

    feat: Implemented useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = ({ method, url, body }) => {
+  console.log("fetch fetching");
+
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState({
+      data: null,
+      loading: true,
+      error: null,
+    });
+
+    fetch(url, {
+      method: method,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setState({
+          loading: false,
+          error: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        console.log(error); // Log any fetch errors
+        setState({
+          loading: false,
+          error: true,
+          data: null,
+        });
+      });
+  }, [url]);
+  return state;
+};
