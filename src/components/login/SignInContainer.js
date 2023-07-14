353576bb77694a1e13be8ae4caf7c3fe38818098
Eismer Lobaina <eismerlobaina@gmail.com>
2023-07-14 14:19:33 -0400

    Refactoriced SignInScreen component component and applying Presentational & Container Component design pattern
    -Divided en two components
    #SignInContainer(  handle the logic and state management)
    #SignInScreen ( focus on rendering the UI based on the provided props. )
    -Fixed other bugs
    
    These changes enhance the clarity and maintainability of the codebase.

---
+++ src/components/login/SignInContainer.js
@@ -0,0 +1,107 @@
+import { SignInScreen } from "./SignInScreen";
+import { useContext, useState } from "react";
+import { useForm } from "../../hooks/useForm";
+import { types } from "../../types/types";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../app/appContext";
+import { Alert, CircularProgress } from "@mui/material";
+
+export const SignInContainer = () => {
+  const navigate = useNavigate();
+  const { dispatch } = useContext(AppContext);
+
+  //const url = "https://kidsrus-backend-node.onrender.com/api/v1/users/login";
+  const localURL = "http://localhost:8787/api/v1/users/login";
+
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const [backDropChildren, setBackDropChildren] = useState({
+  
+    children: <Alert severity="info"></Alert>,
+  });
+  const {  children } = backDropChildren;
+
+
+  const [formValues, handdleInputChange] = useForm({
+    username: "",
+    password: "",
+    repassword: "",
+    email: "",
+    number: "",
+  });
+
+  const { username, password } = formValues;
+
+
+ 
+  const startFeedback = (e) => {
+    setOpenDialog(true);
+    setBackDropChildren({
+      children: <CircularProgress color="inherit" />,
+    });
+  };
+
+  const handleSignIn = (e) => {
+    startFeedback();
+    console.log('singin')
+    e.preventDefault();
+
+    fetch(localURL, {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+
+        setBackDropChildren({
+          children: <Alert severity="success">{data}</Alert>,
+        });
+
+        if (data.user) {
+          const action = {
+            type: types.login,
+            payload: {
+              ...data.user,
+            },
+          };
+          dispatch(action);
+
+          const lastPath = localStorage.getItem("lastPath") || "/";
+          navigate(lastPath, {
+            replace: true,
+          });
+        }
+
+        //setOpenDialog(false);
+      })
+      .catch((error) => {
+        setBackDropChildren({
+          children: <Alert severity="error">An error has occurred</Alert>,
+        });
+       // console.log("error:");
+       // console.log(error);
+      });
+  };
+
+  return (
+    <SignInScreen
+      openDialog={openDialog}
+      setOpenDialog={setOpenDialog}
+      children={children}
+      handleSignIn={handleSignIn}
+      username={username}
+      password={password}
+      handdleInputChange={handdleInputChange}
+      startFeedback={startFeedback}
+    />
+  );
+};
--- src/components/login/SignInScreen.jsx
+++ src/components/login/SignInScreen.jsx
@@ -1,84 +1,36 @@
-import React, { useContext, useState } from "react";
-import { useForm } from "../../hooks/useForm";
-import { types } from "../../types/types";
-import { useNavigate } from "react-router-dom";
-import { AppContext } from "../../app/appContext";
+import { NavLink } from "react-router-dom";
 import SimpleBackdrop from "../feedBack/SimpleBackDrop";
 
-export const SignInScreen = () => {
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const [formValues, handdleInputChange] = useForm({
-    username: "",
-    password: "",
-    repassword: "",
-    email: "",
-    number: "",
-  });
-
-  const { username, password, repassword, email, number } = formValues;
-
-  const url = "https://kidsrus-backend-node.onrender.com/api/v1/users/login";
-
-  const navigate = useNavigate();
-  const { user, dispatch } = useContext(AppContext);
-
-  const handleSignUp = (e) => {
-    e.preventDefault();
-
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-       // console.log(data.user);
-
-            const action = {
-              type: types.login,
-              payload: {
-                ...data.user,
-              },
-            };
-            dispatch(action);
-
-            const lastPath = localStorage.getItem("lastPath") || "/";
-            navigate(lastPath, {
-              replace: true,
-            });
-
-        setOpenDialog(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
+export const SignInScreen = ({
+  openDialog,
+  setOpenDialog,
+  children,
+  handleSignIn,
+  username,
+  password,
+  handdleInputChange,
+  startFeedback,
+}) => {
   return (
     <div className="container text-center">
-      {openDialog && <SimpleBackdrop />}
-
+      <SimpleBackdrop open={openDialog} setOpen={setOpenDialog}>
+        {" "}
+        {children}{" "}
+      </SimpleBackdrop>
       <h1 className="font_francois_one color_mate_blue">Sign Up</h1>
 
       <hr />
       <div className="d-flex align-items-center justify-content-center">
         <div className="row">
           <div className="col-12">
-            <form className="m-2 font_quicksand" onSubmit={handleSignUp}>
+            <form className="m-2 font_quicksand" onSubmit={handleSignIn}>
               <input
+               required
                 type="text"
                 name="username"
                 className="my-3 form-control "
                 placeholder="Type your username"
-                autoComplete="off"
+                autoComplete="on"
                 value={username}
                 onChange={handdleInputChange}
               />
@@ -94,10 +46,12 @@ export const SignInScreen = () => {
               /> */}
 
               <input
+              required
                 type="password"
                 name="password"
                 className="my-3 form-control"
-                placeholder="Repeat your password"
+                placeholder="Type your password"
+                autoComplete="on"
                 value={password}
                 onChange={handdleInputChange}
               />
@@ -111,7 +65,7 @@ export const SignInScreen = () => {
               /> */}
               <div className="bg_color_mate_blue p-1 flex-grow-1">
                 <button
-                  onClick={() => setOpenDialog(true)}
+                 
                   type="submit"
                   className="submit_btn border_white_dashed_own  rounded-0 btn text-light font_francois_one w-100  "
                 >
@@ -119,6 +73,19 @@ export const SignInScreen = () => {
                 </button>
               </div>
             </form>
+            <NavLink
+              className="color_light_blue text-decoration-none font_quicksand"
+              to={"/"}
+            >
+              <span className="text-end">Keep as guess</span>
+            </NavLink>
+            <br />
+            <NavLink
+              className="color_light_blue text-decoration-none font_quicksand"
+              to={"/signup"}
+            >
+              <span>Don't have an account</span>
+            </NavLink>
           </div>
         </div>
       </div>
