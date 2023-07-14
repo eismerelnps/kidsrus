20c4d41da355b93e8f790704d9dab690a22034e3
Eismer Lobaina <eismerlobaina@gmail.com>
2023-07-14 15:47:41 -0400

    fix: On Header component added img-fluid class to the App main image, this class make the image fluid

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -4,65 +4,92 @@ import logo from "../../assets/main/logo.png";
 //import logo2 from "../../assets/logo.png";
 
 import "./ui.css";
-import { DrawerAppBar } from "../newArrivals/navbar/DrawerAppBar";
+//import { DrawerAppBar } from "../newArrivals/navbar/DrawerAppBar";
 import { useContext, useState } from "react";
 import { AppContext } from "../../app/appContext";
-import { Avatar, Icon } from "@mui/material";
+import { Avatar } from "@mui/material";
 import { deepOrange } from "@mui/material/colors";
 import AlertDialog from "../feedBack/AlertDialog";
 import { getProductById } from "../../helpers/getProductById";
+import { types } from "../../types/types";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
-  const { user } = useContext(AppContext) ;
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AppContext);
 
-  
-   //console.log(user.wishList.items)
-   const wishListProducts = user.wishList.items.map((id) => (
-    getProductById(id)
-  ))
-  const cartProducts = user.cart.items.map((product) => (
+  //console.log(user.wishList.items)
+  const wishListProducts = user.wishList.items.map((id) => getProductById(id)) ;
+  const cartProducts = user.cart.items.map((product) =>
     getProductById(product.id)
-  ))
-  
+  ) ;
+
   const [wishList, setWishList] = useState(false);
   const [cart, setCart] = useState(false);
-  
- 
 
- 
+  const handleLogOut = () => {
+    const action = {
+      type: types.logout,
+      payload: {
+        cart: { count: 0, items: [] },
+        wishList: { count: 0, items: [] },
+      }
+    };
+    dispatch(action);
+
+    navigate("/signin", {
+      replace: true,
+    });
+  };
 
   return (
     <div className="mb-5">
-     {wishList &&  <AlertDialog title={'Wish List'} description={wishListProducts} open= {wishList} setOpen={setWishList}/>}
-     {cart &&  <AlertDialog title={'Cart'} description={cartProducts} open= {cart} setOpen={setCart}/>}
+      {wishList && (
+        <AlertDialog
+          title={"Wish List"}
+          description={wishListProducts}
+          open={wishList}
+          setOpen={setWishList}
+        />
+      )}
+      {cart && (
+        <AlertDialog
+          title={"Cart"}
+          description={cartProducts}
+          open={cart}
+          setOpen={setCart}
+        />
+      )}
 
       <div className="text-center">
-        <img src={logo} className="img mb-5" alt="page_logo" />
+        <img src={logo} className="img-fluid mb-5" alt="page_logo" />
       </div>
-      <Avatar sx={{ bgcolor: deepOrange[500] }}></Avatar>
+      <Avatar sx={{ bgcolor: deepOrange[500] }} onClick={handleLogOut}></Avatar>
       <div className="d-flex justify-content-center">
+        <i
+          className="fa-regular fa-heart p-2 mx-1 color_mate_blue position-relative "
+          onClick={() => setWishList(true)}
+        >
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {user.wishList.count}
+            <span className="visually-hidden">Products in wish-list</span>
+          </span>
+        </i>
 
+        <i
+          className="fa-solid fa-cart-shopping p-2 mx-1 color_mate_blue position-relative"
+          onClick={() => setCart(true)}
+        >
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {user.cart.count}
+            <span className="visually-hidden">Products in cart</span>
+          </span>
+        </i>
 
-      <i className="fa-regular fa-heart p-2 mx-1 color_mate_blue position-relative "  onClick={() => setWishList(true)}>
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          {user.wishList.count}
-          <span className="visually-hidden">Products in wish-list</span>
-        </span>
-      </i>
-
-      <i className="fa-solid fa-cart-shopping p-2 mx-1 color_mate_blue position-relative" onClick={() => setCart(true)}>
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          {user.cart.count}
-          <span className="visually-hidden">Products in cart</span>
-        </span>
-      </i>
-
-      <i className="fa-solid fa-magnifying-glass p-2 mx-1 color_mate_blue"></i>
-      {/* <i class="fa-solid fa-user-tie p-2 mx-1 color_mate_blue"></i> */}
-     
-     
+        <i className="fa-solid fa-magnifying-glass p-2 mx-1 color_mate_blue"></i>
+        {/* <i class="fa-solid fa-user-tie p-2 mx-1 color_mate_blue"></i> */}
       </div>
-      
+
       <ul className="nav justify-content-center mb-5 ">
         {nav.map(({ title, navTo }, index) => (
           <NavItem key={title} title={title} navTo={navTo} index={index} />
